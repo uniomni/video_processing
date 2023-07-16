581eafbae9e914e4af6419fbf296926fb4c5b128
Ole Nielsen <Ole.Moller.Nielsen@gmail.com>
2023-07-16 14:10:18 +1000

    Added workaround for ffmpeg not found since November 2022

---
+++ reaper_with_ffmpeg.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+	
+# Starting Reaper with a custom load library (addressing 2022 bug where Reaper can’t render using native video libraries). See https://forums.cockos.com/showthread.php?p=2617432
+
+LD_LIBRARY_PATH=~/ffmpeg/lib reaper
