35cc05fe2d7f182837af6a2f2eceeaf5a737349f
Ole Nielsen <ole.moller.nielsen@gmail.com>
2024-08-22 17:55:53 +1000

    Hack to use specific version of ffmpeg

--- reaper_with_ffmpeg.sh
+++ reaper_with_ffmpeg.sh
@@ -2,4 +2,5 @@
 	
 # Starting Reaper with a custom load library (addressing 2022 bug where Reaper can’t render using native video libraries). See https://forums.cockos.com/showthread.php?p=2617432
 
-LD_LIBRARY_PATH=~/ffmpeg/lib reaper
+LD_LIBRARY_PATH=~/ffmpeg/lib ~/bin/REAPER/reaper
+
