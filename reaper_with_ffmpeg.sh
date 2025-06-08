b1100814faa6c9d65a3fbb254ec9614ed5963065
Ole Nielsen <ole.moller.nielsen@gmail.com>
2025-06-08 18:09:14 +1000

    Forgot to push this change

--- reaper_with_ffmpeg.sh
+++ reaper_with_ffmpeg.sh
@@ -2,5 +2,5 @@
 	
 # Starting Reaper with a custom load library (addressing 2022 bug where Reaper can’t render using native video libraries). See https://forums.cockos.com/showthread.php?p=2617432
 
-LD_LIBRARY_PATH=~/ffmpeg/lib ~/bin/REAPER/reaper
+LD_LIBRARY_PATH=~/ffmpeg/lib ~/opt/REAPER/reaper
 
